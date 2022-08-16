aa79a39c291ce5b564c54325e24b6d2336dfb872
Gemgabriel1 <101466218+Gemgabriel1@users.noreply.github.com>
2022-08-16 22:28:26 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,51 +1,43 @@
 #include "sort.h"
 /**
-* _swap - swap two numbers
-
- * @a: integer 
- * @b: integer 
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
  **/
 void _swap(int *a, int *b)
 {
-int tmp;
-tmp = *a; 
-  *a = *b; 
-  *b = tmp;
-} 
+	inttmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 /**
-* selection_sort - sort array using selection sort algorithm 
-* @array: array
-
+ * selection_sort - sort array using selection sort algorithm
+ * @array: array
  * @size: array size
+ **/
 
-**/
-
-void selection_sort(int *array, size_t size) 
+void selection_sort(int *array, size_t size)
 {
+	unsigned inti, j, min;
 
-unsigned int i, j, min;
+	if (array == NULL || size < 2)
+		return;
 
-if (array == NULL || size < 2)
-  return;
+	for (i = 0; i< size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			_swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
 
-for (i = 0; i < size; i++)
-{
-
-min = i;
-
-for (j = i + 1; j < size; j++)
-{
-
-if (array[min] > array[j]) min = j;
-
-}
-
-if (min != i) 
-{
-
-_swap(&array[i], &array[min]); 
-  print_array(array, size);
-
-}
-} 
 }
