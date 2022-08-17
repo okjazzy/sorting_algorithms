aab08fc0c38ae9b4fd589a7d40e22f83ce3451f8
okjazzy <101530315+okjazzy@users.noreply.github.com>
2022-08-17 10:49:32 +0100

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,89 +1,79 @@
 #include "sort.h"
 
 /**
- * _swap - swap two numbers.
- * @a: integer
- * @b: integer
- **/
-
-void _swap(int *a, int *b)
+ * quick_sort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @size: length of the array
+ * @array: array to be sorted
+ */
+void quick_sort(int *array, size_t size)
 {
-	inttmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-
+	qsorter(array, 0, size - 1, size);
 }
-
 /**
- * _split - Split the array and takes the last element as pivot
- * @arr: Array
- * @min: first element
- * @last: The last element
- * @size: size
- * Return: integer
- **/
-int _split(int *arr, int min, int last, size_t size)
+ * qsorter - a recurssive function that ensures the
+ * array is sorted
+ * @arr: array to be sorted
+ * @f_index: first index of the array
+ * @size: length of the array
+ * @l_index: last index of the array
+ */
+void qsorter(int *arr, int f_index, int l_index, size_t size)
 {
-	intpiv;
-	inti = (min);
-	int j;
+	int c_index;
 
-	piv = arr[last];
-	for (j = min; j < last; j++)
+	if (f_index < l_index)
 	{
-		if (arr[j] <= piv)
+		c_index = partition(arr, f_index, l_index, size);
+
+		qsorter(arr, f_index, c_index - 1, size);
+		qsorter(arr, c_index + 1, l_index, size);
+	}
+}
+/**
+ * partition - function that separates the array into two parts
+ * @arr: array to be partitioned
+ * @f_index: the first index of the section of the array to be partitioned
+ * @l_index: the last index of the section of the array to be partitioned
+ * @size: length of the array
+ * Return: the index of the pivot after it is properly positioned
+ */
+int partition(int *arr, int f_index, int l_index, size_t size)
+{
+	int pivot = arr[l_index];
+	int c_index = f_index;
+	int i;
+
+	for (i = f_index; i < l_index; i++)
+	{
+		if (arr[i] <= pivot)
 		{
-
-			_swap(&arr[i], &arr[j]);
-
-
-			if (i != j)
+			if (c_index < i)
+			{
+				Swap(arr, i, c_index);
 				print_array(arr, size);
-
-			i++;
-
+			}
+			c_index++;
 		}
 	}
 
-	_swap(&arr[i], &arr[last]);
-	if (i != j)
-		print_array(arr, size);
-
-	return (i);
-}
-
-/**
- * quick_sort_array - quick_sort_array
- * @arr: arr
- * @min: min
- * @last: last
- * @size: size
- * Return: Nothing
- */
-void quick_sort_array(int *arr, int min, int last, size_t size)
-{
-
-	intpiv;
-
-	if (min < last)
+	if (arr[c_index] > pivot)
 	{
-		piv = _split(arr, min, last, size);
-		quick_sort_array(arr, min, (piv - 1), size);
-		quick_sort_array(arr, (piv + 1), last, size);
+		Swap(arr, c_index, l_index);
+		print_array(arr, size);
 	}
+	return (c_index);
 }
-
 /**
- * quick_sort -Sort an array using quick_sort algorithm
- * @array: array
- * @size: size
- **/
-void quick_sort(int *array, size_t size)
+ * Swap - swaps the element at two array indexes
+ * @a: index of element to be swapped
+ * @b: index of element to be swapped
+ * @arr: the array whose index is to be swapped
+ */
+void Swap(int *arr, int a, int b)
 {
-	if (size < 2)
-		return;
+	int temp = arr[a];
 
-	quick_sort_array(array, 0, size - 1, size);
+	arr[a] = arr[b];
+	arr[b] = temp;
 }
